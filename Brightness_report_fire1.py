8a4be23cc9181105bcae0916d87e75227726d941
Josh Grant <Josh_grant1@baylor.edu>
2021-10-10 19:40:40 -0500

    finished both brightness reports

--- Brightness_report_fire1.py
+++ Brightness_report_fire1.py
@@ -2,32 +2,33 @@ import json
 from plotly.graph_objs import Scattergeo, Layout
 from plotly import offline
 
-with open("US_fires_9_1.json") as data_file:
-    fire = json.load(data_file)
+infile= open("US_fires_9_1.json", 'r')
+
+fire = json.load(infile)
 
 
-flames, longs, lats = [], [], []
+flames, lons, lats = [], [], []
 
 
 for i in fire:
     flame = i['brightness']
-    long = i['longitude']
+    lon = i['longitude']
     lat = i['latitude']
 
     if flame >= 450:
         flames.append(flame)
-        longs.append(long)
+        lons.append(lon)
         lats.append(lat)
 
 print(flames[:5])
-print(longs[:5])
+print(lons[:5])
 print(lats[:5])
 
 
 fire = [{
     'type':'scattergeo',
-    'long':'longs',
-    'lat':'lats',
+    'lon':lons,
+    'lat':lats,
     'marker':{
         'size':[flame % 5 for flame in flames],
         'color':flames,
@@ -38,5 +39,6 @@ fire = [{
 
 my_layout = Layout(title="Fire Brightness Report 9/1 to 9/13")
 
-fig = {"Data": fire, "Layout": my_layout}
+fig = {'data': fire, 'layout': my_layout}
+
 offline.plot(fig, filename="Firebrightnessreport1.html")
--- Brightness_report_fire2.py
+++ Brightness_report_fire2.py
@@ -2,21 +2,22 @@ import json
 from plotly.graph_objs import Scattergeo, Layout
 from plotly import offline
 
-with open("US_fires_9_14.json") as data_file_2:
-    fire_2 = json.load(data_file_2)
+infile = open("US_fires_9_14.json", 'r')
+    
+fire_2 = json.load(infile)
 
 
-flames_2, longs_2, lats_2 = [], [], []
+flames_2, lons_2, lats_2 = [], [], []
 
 
 for i in fire_2:
     flame_2 = i['brightness']
-    long_2 = i['longitude']
+    lon_2 = i['longitude']
     lat_2 = i['latitude']
 
     if flame_2 >= 450:
         flames_2.append(flame_2)
-        longs_2.append(long_2)
+        lons_2.append(lon_2)
         lats_2.append(lat_2)
 
 #print(flames[:5])
@@ -26,8 +27,8 @@ for i in fire_2:
 
 fire_2 = [{
     'type':'scattergeo',
-    'long':'longs_2',
-    'lat':'lats_2',
+    'lon':lons_2,
+    'lat':lats_2,
     'marker':{
         'size':[flame % 5 for flame in flames_2],
         'color':flames_2,
@@ -38,5 +39,5 @@ fire_2 = [{
 
 my_layout = Layout(title="Fire Brightness Report 9/14 to 9/21")
 
-fig = {"Data": fire_2, "Layout": my_layout}
+fig = {"data": fire_2, "layout": my_layout}
 offline.plot(fig, filename="Firebrightnessreport2.html")
